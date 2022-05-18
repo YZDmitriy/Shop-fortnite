8a157783f08a5f00a422bc767a0ccdabbf8f36e3
Dmitriy <dyuzef@mail.ru>
2022-05-18 09:21:03 +0300

    fix usereducer

--- src/components/Context/Context.js
+++ src/components/Context/Context.js
@@ -16,17 +16,29 @@ export const ContextProvider = ({ children }) => {
 
   const [value, dispatch] = useReducer(reducer, initialState)
 
-  // const value = {
-  //   example: 'hello from context'
-
-  // }
-  value.closeAlert = () => {
-    dispatch({type: 'CLOSE_ALERT'})
+  value.addToBasket = (item) => {
+    dispatch({ type: 'ADD_TO_BASKET', payload: item })
   }
 
   value.removeFromBasket = (itemId) => {
-    dispatch({type: 'REMOVE_FROM_BASKET', payload: {id: itemId}})
+    dispatch({ type: 'REMOVE_FROM_BASKET', payload: { id: itemId } })
 
+  }
+
+  value.incrimentQuantity = (itemId) => {
+    dispatch({ type: 'INCREMENT_QUANTITY', payload: { id: itemId } })
+  }
+
+  value.decrimentQuantity = (itemId) => {
+    dispatch({ type: 'DECREMENT_QUANTITY', payload: { id: itemId } })
+  }
+
+  value.handleBasketShow = () => {
+    dispatch({type: 'TOGGLE_BASKET'})
+  }
+
+  value.closeAlert = () => {
+    dispatch({ type: 'CLOSE_ALERT' })
   }
 
   return (
